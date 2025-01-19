6a313893f2669e27bb5e81ca2163b67177dc64f2
Minkyung Lim <alsrudco1@gmail.com>
2025-01-19 23:09:59 +0900

    project setting

---
+++ src/main/java/com/teamjm/remade/RemadeApplication.java
@@ -0,0 +1,13 @@
+package com.teamjm.remade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamjm/remade/RemadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamjm.remade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
